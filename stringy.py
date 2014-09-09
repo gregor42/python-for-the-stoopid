b4b8a90301527b02e6ea9c17792b02be95810562
gregor42 <gregor42@mail.com>
2014-09-09 15:45:56 -0400

    More fun with numbers & strings

---
+++ stringy.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+#stringy.py
+
+s1 = 'abcdefghijklmnopqrstuvwxyz'
+
+s2 = s1[12:21]
+
+
+print 1
+print s1
+print 2
+print s2
+print 3
+# this will not work
+print s1[-10:-20]
+print 4
+print s1[-20:-10]
+
