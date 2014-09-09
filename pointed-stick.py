f3f24c1193c059ce68ff75e2dcd4b9cc21c850aa
gregor42 <gregor42@mail.com>
2014-09-09 19:15:59 -0400

    Fun with methods on built-in types.  As a java guy this comes a little too easily.

--- pointed-stick.py
+++ pointed-stick.py
@@ -7,4 +7,22 @@ print(os.getlogin())
 print('whereami?')
 print(os.getcwd())
 
+s = 'This is a title that is being centered'
+print(s.center(80))
+
+print(s.upper().center(80))
+
+
+s1 = 'This string will be reversed.'
+print s1
+
+print s1[::-1]
+
+
+s2 = s1.replace('d.','yacious!')[::-1]
+
+print s2
+
+print 'Done.'
+
 
