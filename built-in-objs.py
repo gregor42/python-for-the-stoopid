f57fc02f78e74e105aac1ac0f2120a3fa2861b7e
gregor42 <gregor42@mail.com>
2014-09-09 09:38:04 -0400

    I explored the existing built in object types a little bit.

---
+++ built-in-objs.py
@@ -0,0 +1,57 @@
+#!/usr/local/bin/python
+
+
+n1 = 1234
+n2 = 3.1415
+n3=3+4j
+n4=0b111
+
+
+# python 3.x ?
+#n5=Decimal(n1)
+#n6=Fraction(n2)
+
+
+print n1
+print n2
+print n3
+print n4
+
+#print n5
+#print n6
+
+
+s1 = 'spam'
+s2 = "Bob's Big Boy"
+s3 = b'a\x01c'
+s4 = u'sp\xc4m'
+
+print s1
+print s2
+print s3
+print s4
+
+l1 = [1,[2,'three'], 4.5]
+l2 = list(range(10))
+
+print l1
+print l2
+
+d1 = {'food': 'spam', 'email': 'spam'}
+d2 = dict(hours=10)
+
+print d1
+print d2
+
+e1 = set('abc')
+e2 = {'a','b','c'}
+
+print e1
+print e2
+
+b1 = True
+b2 = False
+
+print b1
+print b2
+
---
+++ maths.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python
+
+
+n1 = 1234
+n2 = 3.1415
+n3=3+4j
+n4=0b111
+
+
+# python 3.x ?
+#n5=Decimal(n1)
+#n6=Fraction(n2)
+
+
+print n1
+print n2
+print n3
+print n4
+
+#print n5
+#print n6
+
