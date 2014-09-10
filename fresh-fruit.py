6f8a96279ece9a142042350fc03db57d86df47b3
gregor42 <gregor42@mail.com>
2014-09-10 16:11:18 -0400

    Mucking about with namespaces, imports string manipulation & list parsing.  Wheee.
    Signed-off-by: gregor42 <gregor42@mail.com>

--- fresh-fruit.py
+++ fresh-fruit.py
@@ -14,14 +14,30 @@ print "import complete."
 print line
 
 
-them = "ex1_3.sh		myfile.py		page14.py		pwd.py			stringy.py		whatsyssy.py brian			fib.py			myfile.pyc		page3.pl		pyfun.py		textadd.py		zen.py built-in-objs.py	hello_world.py		newline.py		page3.py		pyfun_lesson.txt	threenames.py early-thrashing.py	maths.py		numlen.py		page3.sh		randy.py		threenames.pyc ex1_2.py		mod-client.py		os.py			pointed-stick.py	reverse_fail.py		touch-timer.py"
-
+them = "ex1_3.sh		myfile.py		page14.py		pwd.py			stringy.py		whatsyssy.py brian			fib.py			myfile.pyc		page3.pl		pyfun.py		textadd.py		zen.py built-in-objs.py	hello_world.py		newline.py		page3.py		pyfun_lesson.txt	threenames.py	early-thrashing.py	maths.py		numlen.py		page3.sh		randy.py		threenames.pyc ex1_2.py		mod-client.py		os.py			pointed-stick.py	reverse_fail.py		touch-timer.py"
+print them
+print line
+those = them.replace(' ',"").replace("\t",",").replace(",,",",").replace(",,",",")
+print those
+print line
+print them.replace("\w",",")
+print line
 d1 = dir(fib)
 print d1
 print line
 
 d2 = dir(randy)
 print d2
+print line
+
+d3 = dir(them)
+print d3
+print line
+
+fib.fib(1000000)
+print line
+li = those.split(',')
+print li
 print line
 
 print 'Done.'
