6b0ef1b8a3c3f8a44ed21778e47fb096d05f624b
gregor42 <gregor42@mail.com>
2014-09-11 11:52:34 -0400

    I seem to be building a presentation library

---
+++ killer_rabbit.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python
+#
+#  killer_rabbit.py - meant to be used as a sandbox for Python 
+#
+import pri
+
+# Say hello to the Nice People
+pri.nt("Start.")
+
+line = (('-='*39)+'-').center(80)
+
+pri.nt(line)
+
+
+pri.nt('%s, eggs, and %s' % ('spam', 'SPAM!'))
+
+
+pri.nt(line)
+
+pri.nt("Done.")
+
+
