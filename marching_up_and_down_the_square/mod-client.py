83ddd1aba45b7dcba0df5a22b5f8253198b341ea
gregor42 <gregor42@mail.com>
2014-09-18 09:52:53 -0400

    a steel framed treehouse

--- marching_up_and_down_the_square/mod-client.py
+++ marching_up_and_down_the_square/mod-client.py
@@ -1,7 +1,15 @@
 #!/usr/local/bin/python
 
 import fib
+import pri
+
+filename = 'mod-client.py'
+# Say hello to the Nice People
+pri.ntc(filename + " : Start.")
+pri.bar()
+
 
 fib.fib(1000000)
 
-
+pri.bar()
+pri.ntc("Done.")
