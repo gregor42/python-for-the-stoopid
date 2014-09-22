c6356e8deb0b203acbf9e296d85bbf73ccb2acf7
gregor42 <gregor42@mail.com>
2014-09-22 10:37:08 -0400

    Moar examples!

--- diction.py
+++ diction.py
@@ -32,6 +32,29 @@ pri.bar()
 pri.ntc(D.get('x',-1))
 
 pri.bar()
+
+Ks = list(D.keys())
+pri.ntc(Ks)
+pri.bar()
+
+Ks.sort()
+pri.ntc(Ks)
+pri.bar()
+
+for key in sorted(D):
+    print(key, '=>', D[key])
+
+pri.bar()
+for c in 'spam spam spam':
+    print(c.upper())
+
+pri.bar()
+x = 4
+while x > 0:
+    print('spam! ' * x)
+    x -= 1
+
+pri.bar()
 pri.ntc("Done.")
 
 
